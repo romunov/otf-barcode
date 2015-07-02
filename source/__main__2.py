d45d40168180ffaca54128587caef6014b10f412
romunov <romunov@gmail.com>
2015-07-02 16:05:38 +0200

    imports image, but only 1/4

--- source/__main__2.py
+++ source/__main__2.py
@@ -6,6 +6,7 @@
 '''
 
 import Tkinter as tk
+from PIL import Image, ImageTk
 
 class otf(tk.Tk):
 
@@ -19,6 +20,7 @@ class otf(tk.Tk):
         # self.labelVariable = None
         self.gobutton = None
         # self.cv = None
+        self.photo = None
         self.initialize()
 
 
@@ -32,18 +34,41 @@ class otf(tk.Tk):
         self.readcode.focus_set()
         self.readcode.selection_range(0, tk.END)
 
+        ## # Define bindings
         # Define button
         self.gobutton = tk.Button(self, text = "Display", command = self.OnConfirm)
+        self.gobutton.bind("<Return>", self.OnReturn) # ok to focus on button using ALT and press Enter
+
+        # Define enter
+        self.readcode.bind("<Return>", self.OnReturn)
+
+        ## Place figure on a canvas
+        # Import photo and create canvas
+        self.photo = Image.open("363.jpg")
+        ph_width, ph_height = self.photo.size
+        self.photo = ImageTk.PhotoImage(self.photo)
+        cv = tk.Canvas(master = self, width = ph_width, height = ph_height)
+
+        # Add image to the canvas
+        cv.create_image(0,0, image = self.photo)
 
         # Place elements on the grid
         self.grid()
-        self.readcode.grid(column = 0, row = 0, sticky = "EW")
+        self.readcode.grid(column = 0, row = 0)
         self.gobutton.grid(column = 1, row = 0)
+        cv.grid(column = 0, row = 1, columnspan = 2)
 
 
+    # Control resizing
     def OnConfirm(self):
         self.readcode.focus_set()
         self.readcode.selection_range(0, tk.END)
+        print(self.readcode.get())
+
+    def OnReturn(self, event):
+        self.readcode.focus_set()
+        self.readcode.selection_range(0, tk.END)
+        print(self.readcode.get())
 
 
 
